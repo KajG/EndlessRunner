7ccb87331b33fd62811af7b8d5b1c00ea040232d
KajG <kaaiman99@hotmail.com>
2016-09-14 10:15:06 +0200

    Gotta Catch them all
    You choose me and i choose you

---
+++ Assets/Movement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    int speed = 2;
+    Vector3 linksRechts = new Vector3();
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("right"))
+        {
+            linksRechts += new Vector3(1 * speed, 0, 0);
+            this.transform.position = linksRechts;
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            linksRechts -= new Vector3(1 * speed, 0, 0);
+            this.transform.position = linksRechts;
+        }
+    }
+}
---
+++ Assets/enemyMovementScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyMovementScript : MonoBehaviour {
+
+    float enemySpeed = 0.03f;
+    float enemyYposition;
+    float enemyXposition;
+    Vector3 enemyMovement = new Vector3();
+    float randomSpawnLocation;// = Random.Range(0f, 1f);
+
+	// Use this for initialization
+	void Start ()
+    {
+        randomSpawnLocation = Random.Range(0f, 1f);
+        if(randomSpawnLocation <= 0.33)
+        {
+            enemyMovement = new Vector3(-2, 5, 0);
+        }
+        if(randomSpawnLocation >= 0.33 && randomSpawnLocation <= 0.66)
+        {
+            enemyMovement = new Vector3(0, 5, 0);
+        }
+        if(randomSpawnLocation >= 0.66)
+        {
+            enemyMovement = new Vector3(2, 5, 0);
+        }
+        //enemyMovement = new Vector3(Random.Range(-2f, 2f), 10, 0);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        enemyMovement -= new Vector3(0, 1 * enemySpeed, 0);
+        this.transform.position = enemyMovement;
+	}
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("hoi :DDD");
+        if(other.gameObject.tag == "player")
+        {
+            Debug.Log("kut2d");
+            this.transform.position += new Vector3(0, 5, 0);
+        }
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
